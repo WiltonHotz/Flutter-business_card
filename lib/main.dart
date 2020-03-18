3c94fc5dd4495d2752e3aa2ba3d7e7f7b260b787
WiltonHotz <v.horwing@gmail.com>
2020-03-18 14:01:54 +0100

    assignment 2 rows and columns

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,54 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp()
+  );
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+//            verticalDirection: VerticalDirection.up,
+//            mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: 100.0,
+                color: Colors.red,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.yellowAccent,
+                  ),
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.green,
+
+                  ),
+                ],
+              ),
+              Container(
+                width: 100.0,
+                color: Colors.blue,
+              ),
+//              Container(
+//                width: double.infinity,
+//                height: 50.0,
+//              ),
+            ]
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
